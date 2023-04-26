5cb0e361ecfec31480e3b261d7bc637190b7b958
myusername <myemail@gmail.com>
2023-04-26 14:27:07 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,10 +2,9 @@
 
 /**
  * _isalpha - Checks for alphabetic character
- * @c: The character to be checked 
+ * @c: The character to be checked
  * Return: 1 for alphabetic character or 0 for anything else
  */
-
 int _isalpha(int c)
 {
 
@@ -13,5 +12,5 @@ if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
 {
 return (1);
 }
-return (0); 
+return (0);
 }
